d9af5b795a3c3b8027060f7bb55e8ad6ab2b78c1
devkamiki <kamiki@disroot.org>
2026-05-27 21:04:18 +0200

    take input for noise reduction strength

--- noisereduction.m
+++ noisereduction.m
@@ -1,5 +1,6 @@
 clear; close all; clc;
 SourceFilename=input('Enter the filename of the audio to be noise reduced (e.g., music-sample.wav): ', 's');
+Strength = input('Enter the noise reduction strength (e.g., 0.08 for moderate reduction): ');
 inputFile = ['inputfiles/', SourceFilename];
-[noise_reduced_audio, fs] = preprocessing(inputFile, 0.08);
+[noise_reduced_audio, fs] = preprocessing(inputFile, Strength);
 audiowrite(['outputs/', SourceFilename, '_noiseReduced.wav'], noise_reduced_audio, fs);
