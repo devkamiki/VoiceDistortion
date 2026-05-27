30b5ab5ab80ba51b39894ac9dcd20de5ae8cf029
devkamiki <kamiki@disroot.org>
2026-05-27 21:03:33 +0200

    take user input for noisereduction

--- noisereduction.m
+++ noisereduction.m
@@ -1,4 +1,5 @@
 clear; close all; clc;
-inputFile = 'inputfiles/music-sample.wav';   
-[noise_reduced_audio, fs] = preprocessing(inputFile, 0.08);  
-audiowrite('outputs/noiseReduced.wav', noise_reduced_audio, fs);
+SourceFilename=input('Enter the filename of the audio to be noise reduced (e.g., music-sample.wav): ', 's');
+inputFile = ['inputfiles/', SourceFilename];
+[noise_reduced_audio, fs] = preprocessing(inputFile, 0.08);
+audiowrite(['outputs/', SourceFilename, '_noiseReduced.wav'], noise_reduced_audio, fs);
