aac5d30ff7fbefadef43c4074e34466195683c9a
Yining Zhang <y.zhang17@student.tue.nl>
2026-05-28 17:34:30 +0200

    gender converter

--- chorus.m
+++ chorus.m
@@ -12,7 +12,7 @@ pitchShifts = [-9,-8, -0.22, -0.10, 0, 0.10, 0.22, 0.35];   % semitones
 
 % Manual delays, same number as pitchShifts
 % Avoid 0 second delay to reduce comb filtering
-delays = [0.008, 0.012, 0.016, 0.020, 0.024, 0.028, 0.032, 0.036];  % seconds
+delays = [0.008, 0.012, 0.016, 0.020, 0.024, 0.028, 0.032];  % seconds
 
 numVoices = length(pitchShifts);
 
